da862d88ee9ccbb175de1452627bc43b026c62fb
Tanzila Sobaiya Swarna <tanzilaswarna39@gmail.com>
2026-04-22 19:13:45 +0600

    Update RescueTeam.java

--- RescueTeam.java
+++ RescueTeam.java
@@ -1,90 +1,92 @@
 import java.util.ArrayList;
-abstract class RescueTeam {
+abstract class RescueTeam{
     String teamName;
 
-    RescueTeam(String teamName) {
+    RescueTeam(String teamName){
         this.teamName = teamName;
     }
-
     abstract void rescueOperation();
 }
-class BoatRescueTeam extends RescueTeam {
-    BoatRescueTeam(String teamName) {
+class BoatRescueTeam extends RescueTeam{
+    BoatRescueTeam(String teamName){
         super(teamName);
     }
 
     @Override
-    void rescueOperation() {
+    void rescueOperation(){
         System.out.println(teamName + " is rescuing victims using boats.");
     }
 }
-class Victim {
+class Victim{
     private String name;
     private int priorityLevel;
 
-    Victim(String name, int priorityLevel) {
+    Victim(String name, int priorityLevel){
         this.name = name;
         setPriorityLevel(priorityLevel);
     }
 
-    public String getName() {
+    public String getName(){
         return name;
     }
-    public int getPriorityLevel() {
+
+    public int getPriorityLevel(){
         return priorityLevel;
     }
-    public void setPriorityLevel(int priorityLevel) {
-        if (priorityLevel >= 1 && priorityLevel <= 5) {
+
+    public void setPriorityLevel(int priorityLevel){
+        if (priorityLevel >= 1 && priorityLevel <= 5){
             this.priorityLevel = priorityLevel;
         } else {
             this.priorityLevel = 1;
         }
     }
 }
-class FloodEmergency {
+
+class FloodEmergency{
     private double waterLevel;
     private String shelterName;
     private ArrayList<Victim> victims = new ArrayList<>();
 
-    FloodEmergency(double waterLevel) {
+    FloodEmergency(double waterLevel){
         this.waterLevel = waterLevel;
     }
-    void addVictim(Victim v) {
+
+    void addVictim(Victim v){
         victims.add(v);
     }
 
-    void showTotalPeople() {
+    void showTotalPeople(){
         System.out.println("Total People: " + victims.size());
     }
 
-    void calculateBoats() {
+    void calculateBoats(){
         int people = victims.size();
         int boats = people / 4;
 
-        if (people % 4 != 0) {
+        if (people % 4 != 0){
             boats++;
         }
         System.out.println("Boats Required: " + boats);
     }
 
-    void showWaterLevel() {
+    void showWaterLevel(){
         System.out.println("Water Level: " + waterLevel + " feet");
     }
 
-    void checkDangerZone() {
-        if (waterLevel > 10) {
+    void checkDangerZone(){
+        if (waterLevel > 10){
             System.out.println("Danger Zone! Immediate rescue required.");
         } else {
             System.out.println("Safe Zone");
         }
     }
 
-    void assignShelter(String shelterName) {
+    void assignShelter(String shelterName){
         this.shelterName = shelterName;
         System.out.println("Shelter Assigned: " + shelterName);
     }
-
-    void showFullReport(RescueTeam team) {
+    void showFullReport(RescueTeam team){
 
         showWaterLevel();
         checkDangerZone();
@@ -95,14 +97,13 @@ class FloodEmergency {
         team.rescueOperation();
     }
 }
-
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
 
         FloodEmergency flood = new FloodEmergency(12.5);
         RescueTeam boatTeam = new BoatRescueTeam("Boat Rescue Team");
 
-        flood.adVictim v1 = new Victim("A", 5);
+            Victim v1 = new Victim("A", 5);
             Victim v2 = new Victim("B", 3);
             Victim v3 = new Victim("C", 4);
             Victim v4 = new Victim("D", 2);
